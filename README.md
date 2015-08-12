20070c5220c7b6deb4a6cb56f083bf7a32e55362
Ben Kurtovic <ben.kurtovic@gmail.com>
2015-08-12 17:44:10 -0400

    Add README and stub for script.

---
+++ README.md
@@ -0,0 +1,14 @@
+This is a English Wikipedia
+[user script](https://en.wikipedia.org/wiki/Wikipedia:User_scripts) to automate
+common
+[templates for discussion](https://en.wikipedia.org/wiki/Wikipedia:Templates_for_discussion)
+operations, including closures and relisting.
+
+It lives on-wiki at
+[\[\[User:The Earwig/tfdclerk.js\]\]](https://en.wikipedia.org/wiki/User:The_Earwig/tfdclerk.js).
+
+To install it, add the following to your
+[common.js](https://en.wikipedia.org/wiki/Special:MyPage/common.js) or
+[skin.js](https://en.wikipedia.org/wiki/Special:MyPage/skin.js) page:
+
+    importScript("User:The Earwig/tfdclerk.js");  // [[User:The Earwig/tfdclerk.js]]
--- tfdclerk.js
+++ tfdclerk.js
@@ -1,3 +1,12 @@
+// Templates for discussion clerk script by [[User:The Earwig]]
+
+// Development and bug reports: https://github.com/earwig/tfdclerk
+
+// To install, add:
+//     importScript("User:The Earwig/tfdclerk.js");  // [[User:The Earwig/tfdclerk.js]]
+// to [[Special:MyPage/common.js]] or [[Special:MyPage/skin.js]]
+
+/*
 Copyright (C) 2015 Ben Kurtovic <ben.kurtovic@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,3 +26,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+*/
+
+// TODO
